3a5619f10391d5ab527bdad738e057019f6df1b4
«Andrey_Istomin» <an.istomin@gmail.com>
2024-02-07 23:46:42 +0600

    решение ДЗ-1

--- HomeTask/Program.cs
+++ HomeTask/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// // See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+
+public class Answer {
+    static void CompareNumbers(int firstNumber, int secondNumber)
+    {
+        // Введите свое решение ниже
+        if (firstNumber == secondNumber)
+        {Console.Write ("Числа равны");}
+        else
+            {
+                if (firstNumber > secondNumber)
+                {Console.Write ("Первое число больше");}
+                else
+                {Console.Write ("второе число больше");}
+            }
+    }
+
+
+  // Не удаляйте и не меняйте метод Main! 
+    static public void Main(string[] args) {
+        int firstNumber, secondNumber;
+
+        if (args.Length >= 2) {
+            firstNumber = int.Parse(args[0]);
+            secondNumber = int.Parse(args[1]);
+        } else {
+           // Здесь вы можете поменять значения для отправки кода на Выполнение
+            firstNumber = 6;
+            secondNumber = 5;
+        }
+
+        // Не удаляйте строки ниже
+        CompareNumbers(firstNumber, secondNumber);
+    }
+}
